50c7e8bdfc36dc46f74a31bfa6ec1fe02ad17da3
Bigno <linbin@lbinin.com>
2022-07-06 00:34:35 +0800

    chore: fxxk css v2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,4 @@
 module.exports = {
-  mode: "jit",
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
@@ -51,9 +50,6 @@ module.exports = {
         },
       },
     },
-  },
-  variants: {
-    extend: {},
   },
   plugins: [require("@tailwindcss/typography")],
 };
