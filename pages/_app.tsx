3f4cd9ede2515afbc0fcd61e42eef71a50438e12
Bigno <linbin@lbinin.com>
2023-03-25 19:26:34 +0800

    feat: add vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,21 @@
 import '@/styles/index.scss'
-import type { AppProps } from 'next/app'
-
 // lightGallery styles
 import 'lightgallery/scss/lightgallery.scss'
 import 'lightgallery/scss/lg-zoom.scss'
 import 'lightgallery/scss/lg-rotate.scss'
 import 'lightgallery/scss/lg-fullscreen.scss'
+
+import type { AppProps } from 'next/app'
+import { Analytics } from '@vercel/analytics/react'
 import { ThemeContextProvider } from '@/context/theme'
+// import GlobalSearch from '@/components/search/GlobalSearch'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeContextProvider>
+      {/*<GlobalSearch />*/}
       <Component {...pageProps} />
+      <Analytics />
     </ThemeContextProvider>
   )
 }
